a5356edc7928d97152d8130916f6f1ac1843da39
EzequielLeonardoLeotta <leotta010@hotmail.com>
2021-10-11 22:57:36 -0300

    Agrego get de usuario

--- functions/index.js
+++ functions/index.js
@@ -12,21 +12,43 @@ admin.initializeApp({
 })
 const db = admin.firestore()
 
-//POST: usuario(nombre: string, apellido: string, usuario: string, contraseña: string, dni: number) => void
+// POST: usuario(nombre: string, apellido: string, usuario: string, contraseña: string, dni: number) => void
 app.post('/api/usuario', (req, res) => {
-    (async () => {
-      try {
-        await db.collection('usuarios').doc().set({
-          nombre: req.body.nombre,
-          apellido: req.body.apellido,
-          usuario: req.body.usuario,
-          dni: req.body.dni
+  (async () => {
+    try {
+      await db.collection('usuarios').doc().set({
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        usuario: req.body.usuario,
+        dni: req.body.dni,
+      })
+      return res.status(200).send()
+    } catch (error) {
+      return res.status(500).send(error)
+    }
+  })()
+})
+
+// GET: usuario(usuario: string, contraseña: string) => Usuario
+app.get('/api/usuario', (req, res) => {
+  (async () => {
+    try {
+      let response = []
+      await db.collection('usuarios')
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            const data = doc.data()
+            response.push(data)
+          })
         })
-        return res.status(200).send()
-      } catch (error) {
-        return res.status(500).send(error)
-      }
-    })()
-  })
+      return res.status(200).send(response)
+    } catch (error) {
+      return res.status(500).send(error)
+    }
+  })()
+})
+
+// PUT: usuario(nombre: string, apellido: string, usuario: string, contraseña: string, dni: number, domicilios[{calle: string, altura: number, codigoPostal: stringl}], teléfono: number, tarjetas[{tipo: string, numero: number}], cuentas[{numeroDeCuenta: number}], saldo: number) => void
 
 exports.app = functions.https.onRequest(app)
